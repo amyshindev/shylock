cef05deaa4ef7a057108308a9e262fd1afe698f1
amyshindev <amysoo02@gmail.com>
2026-07-09 16:38:48 +0900

    fix: use legacy-docker-compatible backend Dockerfile for aws2 deploy

--- scripts/deploy-aliases.sh
+++ scripts/deploy-aliases.sh
@@ -3,12 +3,14 @@
 # Load from ~/.bash_aliases:
 #   source "$HOME/shylock/scripts/deploy-aliases.sh"
 
-_shylock_remote_build_env='export DOCKER_BUILDKIT=1 COMPOSE_DOCKER_CLI_BUILD=1'
-
 shylock-deploy() {
-  ssh aws2 "${_shylock_remote_build_env} && cd /home/ubuntu/shylock && git pull && docker-compose up -d --build"
+  ssh aws2 'cd /home/ubuntu/shylock && git pull && docker image prune -f && docker-compose up -d --build'
 }
 
 shylock-deploy-fresh() {
-  ssh aws2 "${_shylock_remote_build_env} && cd /home/ubuntu/shylock && git pull && docker-compose build --no-cache --pull && docker-compose up -d"
+  ssh aws2 'cd /home/ubuntu/shylock && git pull && docker image prune -af && docker-compose build --no-cache --pull && docker-compose up -d'
+}
+
+shylock-server-clean() {
+  ssh aws2 'docker system prune -af && docker volume prune -f'
 }
